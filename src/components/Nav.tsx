c690398e167e559ae22b83b9babe5b85858eb038
Souad-HB <souadsalahh@gmail.com>
2024-10-09 09:18:30 -0400

    finished Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,31 @@
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
+
+  const navItems = [
+    { name: "Home", path: "/" },
+    { name: "Potential Candidates", path: "/SavedCandidates" },
+  ];
   return (
-    <div>Nav</div>
-  )
+    <div>
+      <ul className="nav">
+        {navItems.map((item) => (
+          <li className="nav-item" key={item.path}>
+            <Link
+              to={item.path}
+              className={
+                currentPage === item.path ? "nav-link active" : "nav-link"
+              }
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Nav;
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+  readonly name: string;
+  readonly username: string;
+  readonly location: string;
+  readonly avatar: string;
+  readonly email: string;
+  readonly htmlUrl: string;
+  readonly company: string;
+  readonly bio: string;
+}
